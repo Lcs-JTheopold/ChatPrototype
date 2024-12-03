7c5f2393b199f2b5f9d08ac0771965d338bf361c
Jakob Theopold <jtheopold@lcs.on.ca>
2024-12-03 08:46:58 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Jakob Theopold on 03.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
